32e1ab607834a4f90752db1cd5d56c9f3b3d73c7
Ian Vaernet <ianvaernet@gmail.com>
2024-04-22 17:25:39 -0300

    Remove not needed resource

--- IaC/base/s3/main.tf
+++ IaC/base/s3/main.tf
@@ -14,13 +14,6 @@ resource "aws_s3_bucket_website_configuration" "website" {
     }
 }
 
-# resource "aws_s3_bucket_ownership_controls" "bucket" {
-#     bucket = aws_s3_bucket.bucket.id
-#     rule {
-#         object_ownership = "BucketOwnerPreferred"
-#     }
-# }
-
 resource "aws_s3_bucket_public_access_block" "bucket" {
     bucket                  = aws_s3_bucket.bucket.id
     block_public_acls       = false
