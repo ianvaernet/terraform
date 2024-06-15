686621ba53c568f1f3b2b48ecc55e8dd1ff4993b
Ian Vaernet <ianvaernet@gmail.com>
2024-06-15 20:44:11 -0300

    Update script

--- IaC/link-base-files.cmd
+++ IaC/link-base-files.cmd
@@ -1,8 +1,8 @@
 @echo off
 setlocal
 
-set /p "target=Enter the path to base folder: "
-set /p "destination=Enter the path to the env folder: "
+set /p "target=Enter the absolute path to base folder: "
+set /p "destination=Enter the absolute path to the env folder: "
 
 for /d %%A in ("%target%\*") do mklink /d "%destination%\%%~nxA" "%%~A"
 mklink "%destination%\main.tf" "%target%\main.tf"
